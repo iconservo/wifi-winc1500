becb86cb7ba5bf0c1a3adb3c122f035619b41285
kert <kaidokert@gmail.com>
2018-10-18 01:16:26 -0700

    Apply clang-format

--- WINC1500TLSSocket.cpp
+++ WINC1500TLSSocket.cpp
@@ -2,18 +2,15 @@
 #include "Timer.h"
 #include "mbed_assert.h"
 
-#define READ_FLAG           0x1u
-#define WRITE_FLAG          0x2u
+#define READ_FLAG 0x1u
+#define WRITE_FLAG 0x2u
 
-nsapi_protocol_t WINC1500TLSSocket::get_proto()
-{
+nsapi_protocol_t WINC1500TLSSocket::get_proto() {
     return NSAPI_TCP;
 }
 
-
-void WINC1500TLSSocket::event()
-{
-    _event_flag.set(READ_FLAG|WRITE_FLAG);
+void WINC1500TLSSocket::event() {
+    _event_flag.set(READ_FLAG | WRITE_FLAG);
 
     _pending += 1;
     if (_callback && _pending == 1) {
@@ -21,27 +18,26 @@ void WINC1500TLSSocket::event()
     }
 }
 
-nsapi_error_t WINC1500TLSSocket::open(WINC1500Interface *stack)
-{
-	_lock.lock();
+nsapi_error_t WINC1500TLSSocket::open(WINC1500Interface* stack) {
+    _lock.lock();
 
-	if (_stack != NULL || stack == NULL) {
-		_lock.unlock();
-		return NSAPI_ERROR_PARAMETER;
-	}
-	_stack = stack;
+    if (_stack != NULL || stack == NULL) {
+        _lock.unlock();
+        return NSAPI_ERROR_PARAMETER;
+    }
+    _stack = stack;
 
-	nsapi_socket_t socket;
-	nsapi_error_t err = stack->socket_open_tls(&socket, get_proto(), 1);
-	if (err) {
-		_lock.unlock();
-		return err;
-	}
+    nsapi_socket_t socket;
+    nsapi_error_t err = stack->socket_open_tls(&socket, get_proto(), 1);
+    if (err) {
+        _lock.unlock();
+        return err;
+    }
 
-	_socket = socket;
-	_event = callback(this, &WINC1500TLSSocket::event);
-	stack->socket_attach(_socket, Callback<void()>::thunk, &_event);
+    _socket = socket;
+    _event = callback(this, &WINC1500TLSSocket::event);
+    stack->socket_attach(_socket, Callback<void()>::thunk, &_event);
 
-	_lock.unlock();
-	return NSAPI_ERROR_OK;
+    _lock.unlock();
+    return NSAPI_ERROR_OK;
 }
--- WINC1500TLSSocket.h
+++ WINC1500TLSSocket.h
@@ -9,32 +9,29 @@
 #include "netsocket/NetworkInterface.h"
 #include "rtos/EventFlags.h"
 
-
 /** WINC1500 TLS socket connection
  */
 class WINC1500TLSSocket : public TCPSocket {
-public:
+   public:
     /** Opens a winc1500 TLS socket
-         *
-         *  Creates a network socket on the network stack of the given
-         *  network interface. Not needed if stack is passed to the
-         *  socket's constructor.
-         *
-         *  @param stack    WINC1500Interface Network stack as target for socket
-         *  @return         0 on success, negative error code on failure
-         */
-    nsapi_error_t open(WINC1500Interface *stack);
+     *
+     *  Creates a network socket on the network stack of the given
+     *  network interface. Not needed if stack is passed to the
+     *  socket's constructor.
+     *
+     *  @param stack    WINC1500Interface Network stack as target for socket
+     *  @return         0 on success, negative error code on failure
+     */
+    nsapi_error_t open(WINC1500Interface* stack);
 
-protected:
+   protected:
     friend class TCPServer;
     friend class NetworkStack;
     friend class Socket;
 
     virtual nsapi_protocol_t get_proto();
     virtual void event();
-
 };
-
 
 #endif
 

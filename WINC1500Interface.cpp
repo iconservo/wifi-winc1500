d9f75cbdd0239f15fd132a41da800e68f833b338
skt@ciklum.com <skt@ciklum.com>
2019-01-15 17:37:49 +0200

    Closing Socket connection after getting https response

--- WINC1500Interface.cpp
+++ WINC1500Interface.cpp
@@ -290,40 +290,35 @@ int WINC1500Interface::find_free_socket() {
 int WINC1500Interface::socket_open_private(void** handle, nsapi_protocol_t proto, bool use_tls) {
     ScopedLock<Mutex> lock(_mutex);
 
-    int socket_id = find_free_socket();
-    if (socket_id == NSAPI_ERROR_NO_SOCKET) {
-        // report error no free socket
-        winc_debug(_winc_debug, "No free socket!");
-    }
-
-    struct WINC1500_socket* socket = &_socker_arr[socket_id];
-
-    if (!socket) {
-        winc_debug(_winc_debug, "pointer to socket is NULL");
-        return NSAPI_ERROR_NO_SOCKET;
-    }
-
-    socket->tls = use_tls;
-    if (use_tls) {
-        // WINC1500 needs for HTTP connection
-        socket->tls = 0;
-        socket->port = 80;
-    } else {
-        // WINC1500 needs for HTTPS connection
-        socket->tls = 1;
-        socket->port = 443;
-    }
-
     /* Initialize socket module. */
     WINC_SOCKET(socketInit)();
     /* Register socket callback function. */
     WINC_SOCKET(registerSocketCallback)(winc1500_socket_cb, winc1500_dnsResolveCallback);
 
-    int idx = WINC_SOCKET(socket)(AF_INET, SOCK_STREAM, socket->tls);
+    int idx = WINC_SOCKET(socket)(AF_INET, SOCK_STREAM, use_tls);
 
     if (idx >= 0) {
-        socket->id = socket_id;
+	
+	struct WINC1500_socket* socket = &_socker_arr[idx];
+
+	if (!socket) {
+		winc_debug(_winc_debug, "pointer to socket is NULL");
+		return NSAPI_ERROR_NO_SOCKET;
+	}
+	socket->id = idx;
+
         winc_debug(_winc_debug, "WINC1500Interface: socket_opened, id=%d\n", socket->id);
+
+	socket->tls = use_tls;
+	if (!use_tls) {
+		// WINC1500 needs for HTTP connection
+		socket->tls = 0;
+		socket->port = 80;
+	} else {
+		// WINC1500 needs for HTTPS connection
+		socket->tls = 1;
+		socket->port = 443;
+	}
 
         socket->addr = 0;
         socket->read_data_size = 0;
